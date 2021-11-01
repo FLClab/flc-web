0ace7feee7e77e313e333e562b40f66d97a50508
Frederic Beaupre <49285197+fredbeaupre@users.noreply.github.com>
2021-11-01 11:17:40 -0400

    fix publication link bug

--- src/Publication.js
+++ src/Publication.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import "./Publication.css";
 
 function Publication({data}){
+
+    useEffect( () => {
+        console.log(data.source);
+    }, []);
+
     return (
         <div className="publication__container">
             <span className="publication__authors">{data.authors}</span>
             <span className="publication__title">{data.title} </span>
-            <a className="publication__link" href={data.link}>{data.journal}</a>
+            <a href={data.source} className="publication__link">{data.journal}</a>
         </div>
     )
 }
