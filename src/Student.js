f5b42a52a1e586446c7f72cd428227cabf06f131
Frederic Beaupre <49285197+fredbeaupre@users.noreply.github.com>
2021-11-01 14:18:16 -0400

    show more/less student info

--- src/Student.js
+++ src/Student.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Student.css";
 
 function Student({img, student, level, info}) {
+    const [showMore, setShowMore] = useState(false);
+    const [showMoreString, setShowMoreString] = useState('Show more');
+
+    const onReadMoreClick = () => {
+        setShowMore(!showMore);
+        showMore ? setShowMoreString('Show more') : setShowMoreString('Show less');
+    }
+
     return (
         <div className="student">
             <img className="student_img" src={img}></img>
             <div className="info__container">
                 <div className="student__name">{student}</div>
                 <div className="student__level">{level}</div>
-                <div className="student__info">{info}</div>
+                <button id="read-more" className="toggle-info__button" onClick={onReadMoreClick}>{showMoreString}</button>
+                {showMore && <div className="student__info">{info}</div>}
             </div>
         </div>
     )
