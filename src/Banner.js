92546c72397a5ef1fc4fc84db867656ee084a2d2
Frederic Beaupre <49285197+fredbeaupre@users.noreply.github.com>
2021-12-16 12:44:39 -0500

    feat(banner): add brief group info under logo

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,17 @@
 import React from "react";
 import logo from './img/logos/logo-flclab_darkbckg.svg';
 import "./Banner.css"
+import strings from "./strings.json";
 
 function Banner() {
     return (
-        <div
-            className="banner"
-            style={{ backgroundImage: `url('${logo}')`, backgroundSize: "contain", backgroundPositionX: "center" }}
-        >
+        <div className="banner__container">
+            <div
+                className="banner"
+                style={{ backgroundImage: `url('${logo}')`, backgroundSize: "contain", backgroundPositionX: "center" }}
+            >
+            </div>
+            <div className="banner__textcontent">{strings.Banner.groupInfo}</div>
         </div>
     );
 }
