bb2b4c778c2257b22159f8664c10874c30682200
Andreanned3 <33556475+Andreanned3@users.noreply.github.com>
2022-01-13 18:30:08 -0500

    Update StudentSet.js

--- src/StudentSet.js
+++ src/StudentSet.js
@@ -9,24 +9,28 @@ function StudentSet({student1, level1, image1, student2, level2, image2, student
                 img={image1}
                 level={level1}
                 student={student1.name}
+                PI={student1.PI}
                 info={student1.info}
             />
             <Student
                 img={image2}
                 level={level2}
                 student={student2.name}
+                PI={student2.PI}
                 info={student2.info}
             />
             <Student
                 img={image3}
                 level={level3}
                 student={student3.name}
+                PI={student3.PI}
                 info={student3.info}
             />
             <Student
                 img={image4}
                 level={level4}
                 student={student4.name}
+                PI={student4.PI}
                 info={student4.info}
             />
         </div>
