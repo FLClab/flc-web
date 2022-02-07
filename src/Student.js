0ed44d3774ccc7e198670af7e8a448fe7d0c0a8b
fredbeaupre <fred.beaupre32@icloud.com>
2022-02-07 13:41:50 -0500

    feat(members): two-line directors

--- src/Student.js
+++ src/Student.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import "./Student.css";
 
-function Student({img, student, level, info, directors}) {
+function Student({ img, student, level, info, director, codirector }) {
     const [showMore, setShowMore] = useState(false);
     const [showMoreString, setShowMoreString] = useState('Show more');
 
@@ -15,7 +15,10 @@ function Student({img, student, level, info, directors}) {
             <img className="student_img" src={img} alt=""></img>
             <div className="info__container">
                 <div className="student__name">{student}</div>
-                <div className="student__director">Director(s): {directors}</div>
+                <div className="director__container">
+                    <span className="student__director">Director: {director}</span>
+                    <span className="student__director">Codirector: {codirector}</span>
+                </div>
                 <div className="student__level">{level}</div>
                 <button id="read-more" className="toggle-info__button" onClick={onReadMoreClick}>{showMoreString}</button>
                 {showMore && <div className="student__info">{info}</div>}
--- src/StudentSet.js
+++ src/StudentSet.js
@@ -13,28 +13,32 @@ function StudentSet({student1, level1, image1,
                 level={level1}
                 student={student1.name}
                 info={student1.info}
-                directors={student1.directors}
+                director={student1.director}
+                codirector={student1.codirector}
             />
             <Student
                 img={image2}
                 level={level2}
                 student={student2.name}
                 info={student2.info}
-                directors={student2.directors}
+                director={student2.director}
+                codirector={student2.codirector}
             />
             <Student
                 img={image3}
                 level={level3}
                 student={student3.name}
                 info={student3.info}
-                directors={student3.directors}
+                director={student3.director}
+                codirector={student3.codirector}
             />
             <Student
                 img={image4}
                 level={level4}
                 student={student4.name}
                 info={student4.info}
-                directors={student4.directors}
+                director={student4.director}
+                codirector={student4.codirector}
             />
         </div>
     )
