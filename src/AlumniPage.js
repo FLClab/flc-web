b2a822c5383798c90c86e508f3d2f3b9f20ae381
Andreanned3 <33556475+Andreanned3@users.noreply.github.com>
2022-08-11 12:53:49 -0400

    Added Philippe's photo

--- src/AlumniPage.js
+++ src/AlumniPage.js
@@ -5,6 +5,7 @@ import img_GabLeclerc from "./img/members/Gabriel_Leclerc.png";
 import img_Philippe from "./img/members/Philippe_Gagnon_crop.png";
 import img_Marie from "./img/members/Marie_crop.jpg";
 import img_Samuel from "./img/members/Samuel_crop.jpg";
+import img_PhilGT from "./img/members/PhilippeGT_crop.jpg";
 import synapses from "./img/gallery/cultures.png";
 import strings from './strings.json'
 import "./AlumniPage.css"
@@ -46,7 +47,7 @@ function AlumniPage(){
                 alumni1={strings.Alumni.Marie}
                 img2={img_Philippe}
                 alumni2={strings.Alumni.Philippe}
-                img3={synapses}
+                img3={img_PhilGT}
                 alumni3={strings.Alumni.PhilippeGT}
                 img4={img_Samuel}
                 alumni4={strings.Alumni.Samuel}
