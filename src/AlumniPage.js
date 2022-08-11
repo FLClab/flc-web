28dad9f6f6a6c11773aaa64ef4e7d64a4039d9a1
fredbeaupre <fred.beaupre32@icloud.com>
2022-08-11 12:11:43 -0400

    fix(Alumni): move summer students to alumni section, arrange photos and set

--- src/AlumniPage.js
+++ src/AlumniPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import AlumniSet from "./AlumniSet";
+import img_Alexandre from "./img/members/Alexandre_Boulay_crop.JPG";
 import img_GabLeclerc from "./img/members/Gabriel_Leclerc.png";
 import img_Emile from "./img/members/LouisEmile_Robitaille.png";
+import img_Mariame from "./img/members/Mariame_Coulibaly.PNG";
+import img_Philippe from "./img/members/Philippe_Gagnon_crop.png";
+import img_Marie from "./img/members/Marie_crop.jpg";
+import img_Samuel from "./img/members/Samuel_crop.jpg";
+import synapses from "./img/gallery/cultures.png";
 import strings from './strings.json'
 import "./AlumniPage.css"
 import {FaRegArrowAltCircleLeft} from 'react-icons/fa';
@@ -18,10 +24,34 @@ function AlumniPage(){
             {strings.Members.alumni}
             </div>
             <AlumniSet
-            img1={img_GabLeclerc}
-            alumni1={strings.Alumni.Gabriel}
-            img2={img_Emile}
-            alumni2={strings.Alumni.LouisEmile}
+                img1={img_Alexandre}
+                alumni1={strings.Alumni.Alexandre}
+                img2={synapses}
+                alumni2={strings.Alumni.Bastian}
+                img3={synapses}
+                alumni3={strings.Alumni.Elodie}
+                img4={synapses}
+                alumni4={strings.Alumni.Gabrielle}
+             />
+             <AlumniSet
+                img1={img_GabLeclerc}
+                alumni1={strings.Alumni.Gabriel}
+                img2={synapses}
+                alumni2={strings.Alumni.LouisEmile}
+                img3={synapses}
+                alumni3={strings.Alumni.MarcAntoine}
+                img4={synapses}
+                alumni4={strings.Alumni.Mariame}
+             />
+             <AlumniSet
+                img1={img_Marie}
+                alumni1={strings.Alumni.Marie}
+                img2={img_Philippe}
+                alumni2={strings.Alumni.Philippe}
+                img3={synapses}
+                alumni3={strings.Alumni.PhilippeGT}
+                img4={img_Samuel}
+                alumni4={strings.Alumni.Samuel}
              />
         </div>
 
--- src/AlumniSet.js
+++ src/AlumniSet.js
@@ -3,7 +3,10 @@ import "./AlumniSet.css";
 import Alumni from "./Alumni"
 
 function AlumniSet({alumni1, img1,
-                    alumni2, img2}){
+                    alumni2, img2,
+                    alumni3, img3,
+                    alumni4, img4,
+                }){
     return (
         <div className="alumni-set__container">
             <Alumni
@@ -13,6 +16,14 @@ function AlumniSet({alumni1, img1,
             <Alumni
                 img={img2}
                 info={alumni2}
+            /> 
+            <Alumni
+                img={img3}
+                info={alumni3}
+            /> 
+            <Alumni
+                img={img4}
+                info={alumni4}
             /> 
         </div>
     )
