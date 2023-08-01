cac22c84983905a88e5b45935539ad87c93abc5d
fredbeaupre <fred.beaupre32@icloud.com>
2023-08-01 10:31:45 -0400

    feat(gallery): fun carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,60 +1,8 @@
 import React, { useState } from 'react';
 import "./Carousel.css";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
-import BassoonHomer from "./img/gallery/BassoonHomer.png"
-import actinSpectrin from "./img/gallery/ActinSpectrin.png";
-import confocalSted from "./img/gallery/confocal-sted-actin.png";
-import cultures from "./img/gallery/cultures.png";
-import Tubulin from "./img/gallery/LIVE610_Tubulin.png";
-import synapticProteins from "./img/gallery/SynapticProteins.png";
-import Fourcolors from "./img/gallery/4colors_VCF_february2022.png";
 
-function Carousel(){
-    const images = [
-
-        {
-            "id": 0,
-            "img": BassoonHomer,
-            "label": "Bassoon (STED, Magenta), Homer (STED, Cyan) and Actin (Confocal, Green)",
-            "credit": "Theresa Wiesner",
-        },
-        {
-            "id": 1,
-            "img": actinSpectrin,
-            "label": "Actin (Green) and Spectrin (Red), confocal (Left) and STED (Right)",
-            "credit": "Flavie Lavoie-Cardinal and Mado Lemieux",
-        },
-        {
-            "id": 2,
-            "img": Tubulin,
-            "label": "Tubulin labelled with Abberior LIVE 610-Tubulin",
-            "credit": "Andréanne Deschênes",
-        },
-                {
-            "id": 3,
-            "img": Fourcolors,
-            "label": "SMI31 (Confocal, magenta), Actin (STED, green), PSD95 (STED, red), Bassoon (STED, blue)",
-            "credit": "Valérie Clavet-Fournier",
-        },
-                {
-            "id":4,
-            "img": confocalSted,
-            "label": "F-Actin confocal (Left) and STED (Right)",
-            "credit": "Flavie Lavoie-Cardinal",
-        },
-                {
-            "id": 5,
-            "img": cultures,
-            "label": "Neuronal cultures, MAP2 (Green), GFAP (Red), Dapi (Blue)",
-            "credit": "Flavie Lavoie-Cardinal",
-        },
-        {
-            "id": 6,
-            "img": synapticProteins,
-            "label": "Synaptic Proteins; PSD95 (STED, Green), Bassoon (STED, Red), Actin (Confocal, Blue)",
-            "credit": "Theresa Wiesner",
-        }
-    ]
+function Carousel({images, credit}){
 
     const [current, setCurrent] = useState(0);
     const length = images.length;
@@ -86,7 +34,7 @@ function Carousel(){
                             (
                             <div className="carousel-image__container">    
                                 <div className="carousel__label">{image.label}</div>
-                                <div className="carousel__credit">Image credit: {image.credit}</div>
+                                {credit && <div className="carousel__credit">Image credit: {image.credit}</div>}
                                 <img className="carousel__image" src={image.img} alt="gallery"/>
                             </div>
                             )
--- src/Layout.js
+++ src/Layout.js
@@ -37,6 +37,16 @@ import strings from "./strings.json";
 import img_Julia from "./img/members/Julia_Chabbert_crop.jpg";
 import img_Katrine from "./img/members/Katrine_crop.JPG";
 import synapses from "./img/gallery/SynapticProteins.png"
+import BassoonHomer from "./img/gallery/BassoonHomer.png"
+import actinSpectrin from "./img/gallery/ActinSpectrin.png";
+import confocalSted from "./img/gallery/confocal-sted-actin.png";
+import cultures from "./img/gallery/cultures.png";
+import Tubulin from "./img/gallery/LIVE610_Tubulin.png";
+import synapticProteins from "./img/gallery/SynapticProteins.png";
+import Fourcolors from "./img/gallery/4colors_VCF_february2022.png";
+import Sledding from "./img/members/Sledding_Group.jpg";
+import Pentathlon from "./img/members/pentathlon.jpeg";
+import vieille from "./img/members/2019.png";
 
 
 function App() {
@@ -46,6 +56,70 @@ function App() {
   const [pubScroll, setPubScroll] = useState(0);
   const [fundScroll, setFundScroll] = useState(0);
   const [contactScroll, setContactScroll] = useState(0);
+
+  const images = [
+
+    {
+        "id": 0,
+        "img": BassoonHomer,
+        "label": "Bassoon (STED, Magenta), Homer (STED, Cyan) and Actin (Confocal, Green)",
+        "credit": "Theresa Wiesner",
+    },
+    {
+        "id": 1,
+        "img": actinSpectrin,
+        "label": "Actin (Green) and Spectrin (Red), confocal (Left) and STED (Right)",
+        "credit": "Flavie Lavoie-Cardinal and Mado Lemieux",
+    },
+    {
+        "id": 2,
+        "img": Tubulin,
+        "label": "Tubulin labelled with Abberior LIVE 610-Tubulin",
+        "credit": "Andréanne Deschênes",
+    },
+            {
+        "id": 3,
+        "img": Fourcolors,
+        "label": "SMI31 (Confocal, magenta), Actin (STED, green), PSD95 (STED, red), Bassoon (STED, blue)",
+        "credit": "Valérie Clavet-Fournier",
+    },
+            {
+        "id":4,
+        "img": confocalSted,
+        "label": "F-Actin confocal (Left) and STED (Right)",
+        "credit": "Flavie Lavoie-Cardinal",
+    },
+            {
+        "id": 5,
+        "img": cultures,
+        "label": "Neuronal cultures, MAP2 (Green), GFAP (Red), Dapi (Blue)",
+        "credit": "Flavie Lavoie-Cardinal",
+    },
+    {
+        "id": 6,
+        "img": synapticProteins,
+        "label": "Synaptic Proteins; PSD95 (STED, Green), Bassoon (STED, Red), Actin (Confocal, Blue)",
+        "credit": "Theresa Wiesner",
+    }
+  ] 
+
+  const funImages = [
+    {
+        "id": 0,
+        "img": Sledding,
+        "label": "",
+    },
+    {
+        "id": 1,
+        "img": Pentathlon,
+        "label": "",
+    },
+    {
+        "id": 2,
+        "img": vieille,
+        "label": "",
+    }
+  ]
 
   let studentSet1 = [
     {"student": strings.Members.Albert, "img": img_Albert},
@@ -191,7 +265,8 @@ function App() {
       <div className="gallery__label" id="gallery-section">
         {strings.Gallery.label}
         {/* <Gallery /> */}
-        <Carousel />
+        <Carousel images={images} credit={true}/>
+        <Carousel images={funImages} credit={false}/>
       </div>
 
       <div className="publications__label" id="publications-section">
