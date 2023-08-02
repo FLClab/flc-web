fff66d4f4f536743668d9171686f80927211c705
Andreanned3 <andreanne.deschenes.1@ulaval.ca>
2023-08-02 16:20:13 -0400

    Changed sled photo to 2021 group photo (sled photo already in fun gallery)

--- src/Intro.js
+++ src/Intro.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import group from "./img/members/Group_2023.png";
-import group_sled from "./img/members/Sledding_Group.jpg";
+import group_2021 from "./img/members/IMG_3364_group_nomask.png";
 import "./Intro.css"
 import strings from "./strings.json"
 
@@ -18,7 +18,7 @@ function Intro(){
     return (
         <div className="container">
             <div className="left" onMouseOver={onmouseover} onMouseOut={onmouseout}>
-                <img src={withMask ? group_sled : group} alt=""></img> 
+                <img src={withMask ? group_2021 : group} alt=""></img> 
             </div>
             <div className="right">
                 <span className="right__title">{strings.Intro.label}</span>
